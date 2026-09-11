3c5626ef3ca134ca3f133d46fa3d187b17df60ac
martforge <martforge@gmail.com>
2026-09-12 05:33:00 +0800

    Build basic TCP port scanner

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
 use std::net::TcpStream;
 
-
 fn main() {
 
     // Title
-    println!("Rust Port Scanner");
-    
+    println!("=== Rust Port Scanner ===");
+
     // Variables
     let target = "127.0.0.1";
-    let port: u16 = 8080;
-    
-    //Print for readability
-    println!("Target: {target}");
-    println!("Port: {port}");
-    
 
-    let address = format!("{target}:{port}");
-    println!("Address: {address}");
-    let result = TcpStream::connect(address);
-    match result {
-        Ok(_) =>{
-            println!("OPEN");
+    // Print for readability
+    println!("Target: {target}");
+
+    for port in 8000..8010 {
+        let address = format!("{target}:{port}");
+        println!("Checking port: {address}");
+
+        let result = TcpStream::connect(&address);
+
+        match result {
+            Ok(_) => {
+                println!("[+] {address} OPEN");
+            }
+            Err(_) => {
+                println!("[-] {address} CLOSED");
+            }
         }
-        Err(_) =>{
-            println!("CLOSED");
-        }
-        
     }
 }
-
